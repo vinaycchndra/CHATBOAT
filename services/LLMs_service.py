e5e85d3162771ad1ea1c0603e6dab61e69446dcc
vinaycchndra <joshi.vinaya122@gmail.com>
2026-08-12 23:52:01 +0530

    added the summarisation code.

--- services/LLMs_service.py
+++ services/LLMs_service.py
@@ -5,23 +5,21 @@ import logging
 
 logger  = logging.getLogger(__name__)
 
-class LLMWrapper: 
+class GeminiLLM: 
     _instance = None 
 
     @classmethod
     def __getLLM(cls): 
         if cls._instance is None: 
             cls._instance = ChatGoogleGenerativeAI(
-                                model="gemini-3.6-flash",
-                                temperature=1.0,
-                                max_tokens=None,
-                                timeout=None,
-                                max_retries=2,
-                            )
+                model="gemini-3.5-flash-lite",  # Ultra-fast and lightweight
+                temperature=0.7,
+                max_retries=2
+            )
         return  cls._instance   
 
     @classmethod
-    async def sendMessageToLLM(cls, userQuestion: str, context: str, chatSummary:Optional[str], lastNChats: Optional[List[tuple[str]]]) -> str: 
+    async def sendMessageToLLM(cls, userQuestion: str, context: str, chatSummary:Optional[str], lastNChats: Optional[List[tuple[str, str]]]) -> str: 
         """
             userQuestion: str -> Question asked by user.
             context: str -> '\n' separated context fetched from vector db
@@ -83,4 +81,56 @@ class LLMWrapper:
             logger.exception(e)
             raise Exception("Something happend while calling to the llm")
 
-        return ai_response.content
+        return ai_response.content[0]["text"]
+
+
+    @classmethod
+    async def summariseWithExistingSummary(cls, existingSummary: Optional[str], lastNChats: Optional[List[tuple[str, str]]]) -> str: 
+        """
+            existingSummary: str -> So far human and ai are interacting on a abnormal scar tissue called Keloids.
+            lastNChats: str ->  Recent chat interaction ex: [
+                                                                ("human", "can you tell me about the keloids ?"), 
+                                                                ("ai", "Unlike typical scars, which flatten and fade over time, keloids grow continuously over months or years and spread beyond the initial wound.")
+                                                            ]
+                
+            """
+
+        if (lastNChats is None or not lastNChats):
+            raise Exception("last N chats can not be empty...")
+
+        # initialising the set of messages and variables.
+        template_input =  {"lastNChats": "\n".join([f"{role}: {text}" for role, text in lastNChats])}
+
+        # Initialising the system prompt.
+        if existingSummary is None or existingSummary == "": 
+            promptTemplate = """You are ai assistant who summarises the sequence of chat into an overall discussion summary between the ai agent and a human.  Please summarise under 200 words. Here is conversation between human and ai\n{lastNChats}."""
+        else:
+            promptTemplate = """You are ai assistant who updated the existing conversation summary with the existing chat sequence between ai and human. Please summarise under 200 words. Here is existing summary:\n {previousSummary} and current sequence of chat between the ai agent and a human is \n{lastNChats}. Please summarise under 200 words."""
+            template_input["previousSummary"] = existingSummary
+
+        # # create the overall prompt 
+        prompt = ChatPromptTemplate.from_template(promptTemplate)
+
+        # call the LLM to summarise
+        try: 
+            res =  await prompt.ainvoke(template_input)
+        except Exception as e: 
+            logger.exception(e)
+            raise Exception("Prompt could not be created.")
+
+        try: 
+            print(res)
+            llm = cls.__getLLM()
+        except Exception as e: 
+            logger.exception(e)
+            raise Exception("Could not initialise the llm.")
+
+        try: 
+            ai_response = await llm.ainvoke(res)
+        except Exception as e: 
+            logger.exception(e)
+            raise Exception("Something happend while calling to the llm")
+
+        return ai_response.content[0]["text"]
+        
+             
