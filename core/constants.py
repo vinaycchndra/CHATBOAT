ee8ba9ea2e0dd9375c18b84e93c35fbdc1401001
vinaycchndra <joshi.vinaya122@gmail.com>
2026-08-20 15:05:21 +0530

    configured the repo

--- core/constants.py
+++
@@ -1,7 +0,0 @@
-import os 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MY_SECRET = os.getenv("MY_SECRET")
-TOEKN_VALIDITY  = int(os.getenv("TOKEN_VALIDITY", 3600))
--- vector_db/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-VECTOR_DB_HOST=os.getenv("VECTOR_DB_HOST", "localhost")
-VECTOR_DB_PORT=os.getenv("VECTOR_DB_PORT", "8000")
-VECTOR_DB_COLLECTION= os.getenv("VECTOR_DB_COLLECTION", "user_input_docs")
